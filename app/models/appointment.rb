88e55031096453035a4509307a8a439026d4a34a
vedantsinghania1990 <vedantsinghania1990@gmail.com>
2014-03-11 17:14:51 -0500

    Added relation to 'crew' model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,5 +2,10 @@ class Appointment < ActiveRecord::Base
   
   belongs_to :customer
   belongs_to :package
+  belongs_to :crew
+  
+  #named scope :appointments_today, lambda {|date|
+    #{ :conditions => {:appointments_today => date}}
+   # }
   
 end
