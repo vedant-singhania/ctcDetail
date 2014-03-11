0883c7aa9157e0e610c1956126f1bfc7eb2edcab
vedantsinghania1990 <vedantsinghania1990@gmail.com>
2014-03-11 03:07:20 -0500

    Clean Schema for database refactoring!

--- db/schema.rb
+++ db/schema.rb
@@ -11,40 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311003250) do
-
-  create_table "appointments", force: true do |t|
-    t.integer  "customer_id"
-    t.integer  "package_id"
-    t.datetime "appointment_date"
-    t.datetime "appointment_time"
-    t.integer  "number_of_cars"
-    t.string   "location"
-    t.string   "location_type"
-    t.string   "gender_of_customer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "appointments", ["customer_id", "package_id"], name: "index_appointments_on_customer_id_and_package_id", using: :btree
-
-  create_table "customers", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "address"
-    t.string   "car"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "packages", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
